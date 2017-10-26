ce1222c0969d8451819656fe093c6b3c1492d641
Renan Sarcinelli <sarcinellirenan@gmail.com>
2017-10-26 20:28:37 +0000

    added error calculations

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,12 +4,17 @@
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
-#include <raspicam/raspicam.h>
+#include <math.h>
+#include "libs/raspicam/raspicam.h"
 using namespace std;
 
 //timer functions
 #include <sys/time.h>
 #include <unistd.h>
+
+#define threshold 50
+#define view_pixel_height 60 //Middle of image
+	
 class Timer{
     private:
     struct timeval _start, _end;
@@ -47,18 +52,25 @@ void saveImage ( string filepath,unsigned char *data,raspicam::RaspiCam &Camera 
 
 int main ( int argc,char **argv ) {
 	size_t nFramesCaptured=100;
+	double error = 0;
 
     raspicam::RaspiCam Camera; //Cmaera object
     //Open camera 
 	Camera.setWidth(320);
 	Camera.setHeight(240);
 	Camera.setFrameRate(90);
-//	Camera.setSensorMode(7);
+	Camera.setSensorMode(7);
 	Camera.setFormat(raspicam::RASPICAM_FORMAT_GRAY);
 	
     cout<<"Opening Camera..."<<endl;
     if ( !Camera.open()) {cerr<<"Error opening camera"<<endl;return -1;}
     //wait a while until camera stabilizes
+    sleep(5);
+    
+    double atans[320];
+    for (int i=0; i<320; i++)
+    	atans[i] = atan(0.0043261*(160.0-i));
+    	
     cout<<"Connected to camera ="<<Camera.getId() <<" bufs="<<Camera.getImageBufferSize( )<<endl;
     unsigned char *data=new unsigned char[  Camera.getImageBufferSize( )];
     Timer timer;
@@ -70,10 +82,35 @@ int main ( int argc,char **argv ) {
     do{
         Camera.grab();
         Camera.retrieve ( data );
-    }while(++i<nFramesCaptured || nFramesCaptured==0);//stops when nFrames captured or at infinity lpif nFramesCaptured<0
+        
+        //median filter //todo
+        /*
+       	for(int i=0; i<320; i++){
+       		int med_vector[49];
+       		for(int j=0; j<49; j++){
+       			
+       	}
+       	*/
+       	for(int i=0; i<320; i++){
+       		data[view_pixel_height*320 + i] = ((data[view_pixel_height*320 + i]) < threshold) ? 0 : 1;
+       	}
+       	
+       	error = 0;
+       	for(int i=0; i<320; i++)
+       		error +=  (double)(data[view_pixel_height*320 + i])*atans[i];
+       	
+       	printf("%lf\n",error);
+       	
+    }while(true);//(++i<nFramesCaptured || nFramesCaptured==0);//stops when nFrames captured or at infinity lpif nFramesCaptured<0
     
     timer.end();
  
+ 	for(int i=0; i<320; i++)
+       		printf(" %lf ",atans[i]);
+       	
+       	
+ 
+	saveImage("imagem2.WebP",data,Camera);
     cerr<< timer.getSecs()<< " seconds for "<< nFramesCaptured<< "  frames : FPS " << ( ( float ) ( nFramesCaptured ) / timer.getSecs() ) <<endl;
 
     Camera.release();
